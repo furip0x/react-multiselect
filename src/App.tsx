2ff702d7eca5149cbbb3b1286f418d97487b37bb
furip0x <furkanipekci@hotmail.com>
2024-06-06 22:33:29 +0300

    add useCallback, useMemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { getCharacters } from './lib/services/rickandmorty/get-characters';
 import { useQuery } from '@tanstack/react-query';
 import MultiSelect, { MultiSelectItemType } from './components/MultiSelect/MultiSelect';
@@ -26,21 +26,26 @@ function App() {
     refetchOnWindowFocus: false,
   });
 
-  function toggleOption(id: number) {
-    setSelected((prev) => {
-      const selectedOption = prev.find((item) => item.id === id);
-      if (selectedOption) {
-        return prev.filter((item) => item.id !== id);
-      }
+  const toggleOption = useCallback(
+    (id: number) => {
+      setSelected((prev) => {
+        const selectedOption = prev.find((item) => item.id === id);
+        if (selectedOption) {
+          return prev.filter((item) => item.id !== id);
+        }
 
-      const newOption = options.find((item) => item.id === id);
-      if (newOption) {
-        return [...prev, newOption];
-      }
+        const newOption = options.find((item) => item.id === id);
+        if (newOption) {
+          return [...prev, newOption];
+        }
 
-      return prev;
-    });
-  }
+        return prev;
+      });
+    },
+    [options]
+  );
+
+  const memoizedOptions = useMemo(() => options, [options]);
 
   useEffect(() => {
     if (charactersData?.data) {
@@ -78,7 +83,7 @@ function App() {
     <div className="grid min-h-dvh w-full justify-center bg-white p-10">
       <div className="w-96">
         <MultiSelect
-          options={options}
+          options={memoizedOptions}
           isOptionsLoading={isCharactersLoading}
           isOptionsError={isCharactersError}
           optionsError={charactersError}
